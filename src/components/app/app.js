2159e895c7a96dc97bb754aec9119df2c91b2bf6
tatianasabanina <tatiana08st@gmail.com>
2020-01-07 19:47:06 +0200

    react patterns

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,10 +5,15 @@ import RandomPlanet from '../random-planet';
 import ErrorButton from '../error-button';
 import ErrorIndicator from '../error-indicator';
 import PeoplePage from '../people-page';
+import ItemList from '../item-list';
+import PersonDetails from '../person-details';
+import SwapiService from '../../services/swapi-service';
 
 import './app.css';
 
 export default class App extends Component {
+
+  swapiService = new SwapiService(); 
 
   state = {
     showRandomPlanet: true,
@@ -54,6 +59,28 @@ export default class App extends Component {
           <ErrorButton />
         </div>
         <PeoplePage /> 
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllPlanets}
+              renderItem={(item) => item.name} />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+        </div>
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllStarships}
+              renderItem={(item) => item.name} />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+        </div>
       </div>
     );
   }
