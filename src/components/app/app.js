213eb8ae0898fda2841aac804c49102f12bf1d87
tatianasabanina <tatiana08st@gmail.com>
2020-01-13 19:25:07 +0200

    react-router, dinamic paths, withRouter, match, history, relative paths, optional parameters

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,8 +8,11 @@ import DummySwapiService from '../../services/dummy-swapi-service';
 import ErrorBoundry from '../error-boundry';
 import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages';
 import { SwapiServiceProvider } from '../swapi-service-context';
+import { StarshipDetails } from '../sw-components';
 
 import './app.css';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 export default class App extends Component {
 
@@ -45,15 +48,26 @@ export default class App extends Component {
     return (
       <ErrorBoundry>
         <SwapiServiceProvider value = {this.state.swapiService}>
-          <div className="stardb-app">
-            <Header onServiceChange={this.onServiceChange} />
+          <Router>
+            <div className="stardb-app">
+              <Header onServiceChange={this.onServiceChange} />
+              
+              <RandomPlanet />
 
-            <RandomPlanet />
-            <PeoplePage />            
-            <PlanetsPage />
-            <StarshipsPage />
-        
-          </div>
+              <Route path="/" exact 
+                render={() => <h2>Welcome to StarDB</h2>} />
+              <Route path="/people/:id?" component={PeoplePage} />
+              <Route path="/planets" component={PlanetsPage} />
+              <Route path="/starships" exact component={StarshipsPage} />
+              <Route path="/starships/:id" 
+                render={({ match, location, history }) => {
+                  const { id } = match.params;
+                  return <StarshipDetails itemId={id} />
+                  } 
+                } />
+
+            </div>
+          </Router>
         </SwapiServiceProvider>
       </ErrorBoundry>
     );
